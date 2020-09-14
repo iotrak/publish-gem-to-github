fe4f75ceb8f46eb92f746a905c3db53f1534ee9a
Mylan Connolly <mconnolly@iotrak.com>
2020-09-14 14:32:07 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ WORKING_DIRECTORY=$3
 
 [ -z "${GITHUB_TOKEN}" ] && { echo "Missing input.token!"; exit 2; }
 [ -z "${OWNER}" ] && { echo "Missing input.owner!"; exit 2; }
+[ -z "${WORKING_DIRECTORY}" ] && { echo "Missing working directory!"; exit 2; }
 
 echo "Setting up access to GitHub Package Registry"
 mkdir -p ~/.gem
@@ -13,7 +14,9 @@ touch ~/.gem/credentials
 chmod 600 ~/.gem/credentials
 echo ":github: Bearer ${GITHUB_TOKEN}" >> ~/.gem/credentials
 
+cd "$WORKING_DIRECTORY"
+
 echo "Building the gem"
-gem build ${WORKING_DIRECTORY:-.}/*.gemspec
+gem build *.gemspec
 echo "Pushing the built gem to GitHub Package Registry"
 gem push --key github --host "https://rubygems.pkg.github.com/${OWNER}" ./*.gem
